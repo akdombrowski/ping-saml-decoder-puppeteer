3771514d3708aca9623ff906669d12e743022a9e
akdombrowski <akdflyinvee73@gmail.com>
2022-01-23 05:38:39 -0600

    remove irrelevant comments

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
-// This is the main Node.js source code file of your actor.
-// It is referenced from the "scripts" section of the package.json file,
-// so that it can be started by running "npm start".
-
-// Import Apify SDK. For more information, see https://sdk.apify.com/
 const puppeteer = require("puppeteer");
 
 (async () => {
